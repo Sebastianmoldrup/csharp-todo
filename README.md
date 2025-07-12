c3768e2615e6fe50e8a5de493b3c6de6c77bd123
sebastian <sebastianmoldrup@Macmini.home>
2025-07-12 02:34:43 +0200

    added readme

---
+++ README.md
@@ -0,0 +1,3 @@
+# Entry level C# TodoApp
+
+Creates an instance where you can run static methods to create, print and exit the instance/loop
--- obj/Debug/net9.0/TodoApp.AssemblyInfo.cs
+++ obj/Debug/net9.0/TodoApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a16b61e08fb0d8c03b31d6657969ccc82a5a9210")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+32d3a1bca9562ad42e556b155df547dd1e6f8e27")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
