32d3a1bca9562ad42e556b155df547dd1e6f8e27
sebastian <sebastianmoldrup@Macmini.home>
2025-07-12 02:32:58 +0200

    improved v2 update

--- Program.cs
+++ Program.cs
@@ -1,36 +1,85 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+// Version 1
+// class TodoApp
+// {
+//     static void Main()
+//     {
+//         Console.WriteLine("Din TodoList");
 
+//         while (true)
+//         {
+//             Console.WriteLine("Use command add/list/exit");
+//             string? command = Console.ReadLine();
+
+//             if (command == "add")
+//             {
+//                 Console.WriteLine("Enter in your new Todo");
+//                 string? todo = Console.ReadLine();
+//                 TaskManager.AddTodo(todo);
+//                Console.WriteLine("Todo has been added!");
+//             }
+//             else if (command == "list")
+//             {
+//                 TaskManager.PrintTodos();
+//             }
+//             else if (command == "exit")
+//             {
+//                 Console.WriteLine("Closing Todo");
+//                 break;
+//             } 
+//             else
+//             {
+//                 Console.WriteLine("Uknown command.");
+//             }
+//         }
+//     }
+// }
+
+// Version 2
 class TodoApp
 {
     static void Main()
     {
-        Console.WriteLine("Din TodoList");
+        Console.WriteLine("Initialized Todo App");
+        Console.WriteLine("type 'help' for commands"); 
 
         while (true)
         {
-            Console.WriteLine("Use command add/list/exit");
-            string? command = Console.ReadLine();
+            string? input = Console.ReadLine();
+            if (string.IsNullOrWhiteSpace(input)) return;
+            string[] parts = input.Split(' ', 2); // Split command + argument
+            string cmd = parts[0];
+            string arg = parts.Length > 1 ? parts[1] : "";
 
-            if (command == "add")
+            if (cmd == "help")
             {
-                Console.WriteLine("Enter in your new Todo");
-                string? todo = Console.ReadLine();
-                TaskManager.AddTodo(todo);
-               Console.WriteLine("Todo has been added!");
+                Console.WriteLine("Use these commands add/list/exit");
             }
-            else if (command == "list")
+            else if (cmd == "add")
             {
+                if (string.IsNullOrWhiteSpace(arg))
+                {
+                    Console.WriteLine("Provide a todo after the add cmd");
+                    continue;
+                }
+
+                TaskManager.AddTodo(arg);
+                Console.WriteLine($"added {arg}");
+            }
+            else if (cmd == "list")
+            {
+                Console.WriteLine("Your todos:");
                 TaskManager.PrintTodos();
             }
-            else if (command == "exit")
+            else if (cmd == "exit")
             {
-                Console.WriteLine("Closing Todo");
+                Console.WriteLine("Exiting Todo");
                 break;
-            } 
+            }
             else
             {
-                Console.WriteLine("Uknown command.");
+                Console.WriteLine($"Uknown cmd: {cmd}");
             }
         }
     }
